bc01d8faeb41ac0c8e6e1d91e00bf478f2fb797e
Novran Faathir <44427665+panophan@users.noreply.github.com>
2019-12-11 15:29:34 +0700

    Update script.bash

--- multi-thread-loop/script.bash
+++ multi-thread-loop/script.bash
@@ -3,7 +3,7 @@
 THREAD=5 # push 5 process simultaneously
 
 (
-	for thing in ((thing=1;thing<=20;thing++)); do 
+	for thing in {1..20}; do 
 		((cthread=cthread%THREAD)); ((cthread++==0)) && wait
 		someFunction "$thing" & 
 	done
